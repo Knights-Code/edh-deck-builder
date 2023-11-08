85cdd826c45f5e4bda3ff222205cb9b808454e1a
Tom Chataway <pugtura@gmail.com>
2023-11-08 18:32:04 +1030

    Base provider class and test

---
+++ EdhDeckBuilder/Model/CardModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdhDeckBuilder.Model
+{
+    public class CardModel
+    {
+        public string Name { get; set; } // Might not need to store this...
+
+        public string MultiverseId { get; set; }
+
+        public Image CardImage { get; set; } = null;
+
+        public string BuildGathererUrl()
+        {
+            return $"https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid={MultiverseId}&type=card";
+        }
+    }
+}
---
+++ EdhDeckBuilder/Tests/CardProviderTests.cs
@@ -0,0 +1,33 @@
+﻿using EdhDeckBuilder.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdhDeckBuilder.Tests
+{
+    [TestFixture]
+    public class CardProviderTests
+    {
+        [Test]
+        public void EndToEnd()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var cardProvider = new CardProvider();
+            cardProvider.Initialise();
+            Console.WriteLine("Initialising database...");
+            stopwatch.Stop();
+            Console.WriteLine($"Database initialisation complete ({stopwatch.ElapsedMilliseconds} ms)");
+            var card = cardProvider.TryGetCard("Ancestor's Chosen");
+            var cardImageUrl = card.BuildGathererUrl();
+            Console.WriteLine($"Gatherer URL: {cardImageUrl}");
+
+            Assert.That(card != null);
+            Assert.AreEqual("https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=130550&type=card", cardImageUrl);
+        }
+    }
+}
