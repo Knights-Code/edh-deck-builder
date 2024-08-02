5d4b3e17b3da7d1c2f101a08623033271651cb9d
Tom Chataway <pugtura@gmail.com>
2024-08-02 14:00:05 +0930

    Add sandbox for web crawler library

---
+++ PuppeteerSandbox/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace PuppeteerSandbox
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fetching browser...");
+            await new BrowserFetcher().DownloadAsync();
+            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+                {
+                    Headless = true
+                }))
+            {
+                Console.WriteLine("Creating page...");
+                var page = await browser.NewPageAsync();
+
+                Console.WriteLine("Navigating to website...");
+                var navigationOptions = new NavigationOptions
+                {
+                    Timeout = 0,
+                    WaitUntil = new[]
+                    {
+                        WaitUntilNavigation.Networkidle2
+                    }
+                };
+                await page.GoToAsync("https://tagger.scryfall.com/card/otc/267", navigationOptions);
+
+                Console.WriteLine("Getting content...");
+                var content = await page.GetContentAsync();
+
+                Console.WriteLine(content);
+                Console.WriteLine($"\n{(content.Contains("ramp") ? "Ramp detected" : "Nothing found")}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
