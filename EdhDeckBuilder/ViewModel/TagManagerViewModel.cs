a4bd1df00f874cd9312c42121d703e4067927905
Tom Chataway <pugtura@gmail.com>
2024-08-06 12:53:57 +0930

    Add filterable list to Manage Tags screen

--- EdhDeckBuilder/ViewModel/TagManagerViewModel.cs
+++ EdhDeckBuilder/ViewModel/TagManagerViewModel.cs
@@ -27,6 +27,13 @@ namespace EdhDeckBuilder.ViewModel
             set { SetProperty(ref _tagsSummary, value); }
         }
 
+        private List<string> _tagSummaries;
+        public List<string> TagSummaries
+        {
+            get { return _tagSummaries; }
+            set { SetProperty(ref _tagSummaries, value); }
+        }
+
         private bool _canRetrieve;
         public bool CanRetrieve
         {
@@ -41,10 +48,34 @@ namespace EdhDeckBuilder.ViewModel
             set { SetProperty(ref _status, value); }
         }
 
-        public ICommand RetrieveCommand { get; set; }
+        private string _errors;
+        public string Errors
+        {
+            get { return _errors; }
+            set { SetProperty(ref _errors, value); }
+        }
 
+        private string _filterInput;
+        public string FilterInput
+        {
+            get { return _filterInput; }
+            set
+            {
+                if (SetProperty(ref _filterInput, value))
+                {
+                    StartOrRestartFilter(value);
+                }
+            }
+        }
+
+        public ICommand RetrieveCommand { get; set; }
+        public ICommand ResetFilterCommand { get; set; }
+
+        private List<string> _fullTagsList;
         private readonly DeckModel _deck;
         private readonly CardProvider _cardProvider;
+        private Task _filterTask;
+        private CancellationTokenSource _filterCancel;
 
         public TagManagerViewModel(string title, DeckModel deck, CardProvider cardProvider)
         {
@@ -53,7 +84,57 @@ namespace EdhDeckBuilder.ViewModel
             CanRetrieve = true;
             _deck = deck;
             _cardProvider = cardProvider;
+            _fullTagsList = new List<string>();
+            TagSummaries = new List<string>();
+            _filterCancel = new CancellationTokenSource();
             RetrieveCommand = new DelegateCommand(async () => await Retrieve());
+            ResetFilterCommand = new DelegateCommand(ResetFilter);
+        }
+
+        public void ResetFilter()
+        {
+            FilterInput = string.Empty;
+        }
+
+        public async void StartOrRestartFilter(string filterText)
+        {
+            if (_filterTask != null && !_filterTask.IsCompleted)
+            {
+                _filterCancel.Cancel();
+                await _filterTask;
+            }
+
+            _filterCancel = new CancellationTokenSource();
+            _filterTask = FilterAsync(filterText, _filterCancel);
+            await _filterTask;
+        }
+
+        public async Task FilterAsync(string filterText, CancellationTokenSource cts)
+        {
+            var filteredList = new List<string>();
+
+            await Task.Run(() =>
+            {
+                foreach (var tagSummary in _fullTagsList)
+                {
+                    if (cts.IsCancellationRequested)
+                    {
+                        break;
+                    }
+
+                    if (tagSummary.ToLower().Contains(filterText.ToLower()))
+                    {
+                        filteredList.Add(tagSummary);
+                    }
+                }
+            });
+
+            if (cts.IsCancellationRequested)
+            {
+                return;
+            }
+
+            TagSummaries = filteredList;
         }
 
         public async Task Retrieve()
@@ -73,16 +154,17 @@ namespace EdhDeckBuilder.ViewModel
             var tagsDictionary = scryfallTagsResult.Item1;
             var errors = scryfallTagsResult.Item2;
             Status = "Tags retrieved. Compiling summary...";
-            TagsSummary = await Task.Run(() => CompileTagsSummary(tagsDictionary, errors));
+            Errors = $"Errors:\n{string.Join("\n", errors)}";
+            TagSummaries = await Task.Run(() => CompileTagsSummary(tagsDictionary));
+            _fullTagsList = TagSummaries;
             Status = $"Retrieval complete! Retrieved tags for {tagsDictionary.Keys.Count} card(s).";
 
             CanRetrieve = true;
         }
 
-        private string CompileTagsSummary(Dictionary<string, List<string>> tagsDictionary,
-            List<string> errors)
+        private List<string> CompileTagsSummary(Dictionary<string, List<string>> tagsDictionary)
         {
-            var result = "";
+            var result = new List<string>();
 
             // Create dictionary of tag counts keyed by tag name.
             var tagSummaries = new Queue<TagSummary>();
@@ -114,18 +196,10 @@ namespace EdhDeckBuilder.ViewModel
                 }
             }
 
-            // Frontload errors.
-            if (errors.Any())
-            {
-                result += "Errors:\n";
-                result += $"{string.Join("\n", errors)}";
-                result += "\n\n";
-            }
-
-            // Print staple counts second.
+            // Print staple counts first.
             for (var i=0; i < 5; i++)
             {
-                result += tagSummaries.Dequeue().ToString() + "\n";
+                result.Add(tagSummaries.Dequeue().ToString() + "\n");
             }
 
             // Sort the rest descending by count.
@@ -134,7 +208,7 @@ namespace EdhDeckBuilder.ViewModel
 
             while (tagSummaries.Any())
             {
-                result += tagSummaries.Dequeue().ToString() + "\n";
+                result.Add(tagSummaries.Dequeue().ToString() + "\n");
             }
 
             return result;
@@ -164,6 +238,7 @@ namespace EdhDeckBuilder.ViewModel
 
             public override string ToString()
             {
+                return $"{Name}: {Count}";
                 return $"{Name}: {Count} ({string.Join(", ", Cards)})";
             }
         }
